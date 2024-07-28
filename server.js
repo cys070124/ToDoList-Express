e87e442be3c40b213bd90d5e20af2adf49a43d1b
cys070124 <cys070124@gmail.com>
2024-07-28 23:59:54 +0900

    DB connect

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const app = express()
+const { MongoClient } = require('mongodb')
+
+let db
+const url = ''
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('')
+}).catch((err)=>{
+  console.log(err)
+})
 
 const PORT = 8080;
 app.listen(PORT, () => {
@@ -9,3 +19,4 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
     res.send('Test')
 })
+
